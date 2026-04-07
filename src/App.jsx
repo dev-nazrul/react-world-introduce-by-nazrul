5ec2beefe61613d4d1281c39da7cee429faa5d4f
dev-nazrul <nazrulislamnu19@gmail.com>
2026-04-07 18:00:02 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+
+import './App.css'
+import Countries from './Countries'
+
+function App() {
+
+
+  return (
+    <>
+     <h1>I Love Allah</h1>
+     <Countries></Countries>
+    </>
+  )
+}
+
+export default App
---
+++ src/Countries.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Countries = () => {
+    return (
+        <div>
+            <h3>May Allah Hepl Me</h3>
+        </div>
+    );
+};
+
+export default Countries;
